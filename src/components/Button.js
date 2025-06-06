e6affe370c8f28724a2e7bddc35e456c49288636
chiranjib-swain <chiranjib-swain@github.com>
2025-06-06 07:32:16 +0530

    for creating a pr

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,5 @@
 // A sample component
 function Button({ label }) {
-    return `<button>${label}</button>`;// thsis is a simple button compohdfdfent test for 870 . issues added hdhsgfdsdfgfggdsdsdgfdgf
+    return `<button>${label}</button>`;// thsis is a simple button compohdfdfent test for 870 . issues added hdhsgfdsdfgfggdsdsdgfdgf970
   }
   module.exports = Button;
